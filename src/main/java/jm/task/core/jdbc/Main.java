4acb685f9e482206076c989c5b0a42475ece91d5
Orm666 <anton.ga4an@yandex.ru>
2023-05-02 15:27:34 +0300

    1_1_4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,7 +10,7 @@ public class Main {
         userService.saveUser("Name1", "LastName1", (byte) 11);
         userService.saveUser("Name2", "LastName2", (byte) 22);
         userService.saveUser("Name3", "LastName3", (byte) 33);
-        userService.saveUser("Name4", "LastName4", (byte) 44);
+        userService.saveUser("Name4", "LastName4", (byte) 45);
         //userService.removeUserById(1);
         System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
